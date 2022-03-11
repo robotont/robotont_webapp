652d346ff468fe4d545c64ca372619d3deaaa451
Veiko Vunder <veix123@gmail.com>
2022-03-11 12:09:50 +0200

    invoking npm build from cmakelists when running catkin build

--- scripts/build.sh
+++ scripts/build.sh
@@ -3,11 +3,7 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 cd $DIR/../backend && npm install
 cd $DIR/../frontend && npm install
-
 cd $DIR/../frontend && npm run build
-mv $DIR/../frontend/dist/ $DIR/../backend/
 
-cd $DIR/../ttyd && mkdir build && cd build
-cmake ..
-make && sudo make install
-catkin build
+rm -fr $DIR/../backend/dist
+mv $DIR/../frontend/dist $DIR/../backend/
--- scripts/ttyd_script.sh
+++ scripts/ttyd_script.sh
@@ -1,4 +1,2 @@
 #!/bin/bash
-
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
-cd $DIR/../ttyd/build && ./ttyd -p 5000 bash
+ttyd -p 5000 bash
